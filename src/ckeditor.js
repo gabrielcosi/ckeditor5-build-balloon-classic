c5b2aedee040800e4115b42d5d680968e6d5b987
gabrielpfl <gabriel@latinamericaforless.com>
2022-03-03 14:50:30 -0500

    added font color, size and family

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -8,53 +8,51 @@ import BalloonEditorBase from "@ckeditor/ckeditor5-editor-balloon/src/balloonedi
 import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 import BalloonBlockEditor from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor";
 
-import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
-import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
-import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
-import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
-import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-import Image from "@ckeditor/ckeditor5-image/src/image";
-import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
-import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
-import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
-import ImageInsert from "@ckeditor/ckeditor5-image/src/imageinsert";
-import Indent from "@ckeditor/ckeditor5-indent/src/indent";
-import Link from "@ckeditor/ckeditor5-link/src/link";
-import List from "@ckeditor/ckeditor5-list/src/list";
-import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
-import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
-import Table from "@ckeditor/ckeditor5-table/src/table";
-import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
-import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
-import Code from "@ckeditor/ckeditor5-basic-styles/src/code";
-import CodeBlock from "@ckeditor/ckeditor5-code-block/src/codeblock";
-import HtmlEmbed from "@ckeditor/ckeditor5-html-embed/src/htmlembed";
+import BalloonEditor from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor.js";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
+import FontFamily from "@ckeditor/ckeditor5-font/src/fontfamily.js";
+import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
+import Image from "@ckeditor/ckeditor5-image/src/image.js";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
+import Link from "@ckeditor/ckeditor5-link/src/link.js";
+import List from "@ckeditor/ckeditor5-list/src/list.js";
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed.js";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js";
+import Table from "@ckeditor/ckeditor5-table/src/table.js";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
 
 class BalloonEditor extends BalloonEditorBase {}
 class ClassicEditor extends ClassicEditorBase {}
 class BalloonBlockEditor extends BalloonBlockEditor {}
 
 const plugins = [
-  Essentials,
-  UploadAdapter,
   Autoformat,
-  Bold,
-  Italic,
   BlockQuote,
-  CKFinder,
-  EasyImage,
+  Bold,
+  Essentials,
+  FontColor,
+  FontFamily,
+  FontSize,
   Heading,
   Image,
   ImageCaption,
   ImageStyle,
   ImageToolbar,
-  ImageInsert,
+  ImageUpload,
   Indent,
+  Italic,
   Link,
   List,
   MediaEmbed,
@@ -63,9 +61,6 @@ const plugins = [
   Table,
   TableToolbar,
   TextTransformation,
-  Code,
-  CodeBlock,
-  HtmlEmbed,
 ];
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = plugins;
@@ -76,6 +71,9 @@ const config = {
   toolbar: {
     items: [
       "heading",
+      "fontColor",
+      "fontSize",
+      "fontFamily",
       "|",
       "bold",
       "italic",
@@ -83,28 +81,23 @@ const config = {
       "bulletedList",
       "numberedList",
       "|",
-      "code",
-      "codeBlock",
+      "outdent",
+      "indent",
       "|",
-      "htmlEmbed",
-      "|",
-      "imageInsert",
+      "imageUpload",
       "blockQuote",
       "insertTable",
       "mediaEmbed",
       "undo",
       "redo",
-      "|",
-      "indent",
-      "outdent",
     ],
   },
   image: {
     toolbar: [
-      "imageStyle:full",
-      "imageStyle:side",
-      "|",
       "imageTextAlternative",
+      "imageStyle:inline",
+      "imageStyle:block",
+      "imageStyle:side",
     ],
   },
   table: {
